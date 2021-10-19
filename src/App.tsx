326a985d876b10023bdf7e18ef36f6150bebdfec
alaedine zorgui <a.zorgui@adiz-data.com>
2021-10-19 17:17:03 +0100

    netlify

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          SLearn React
         </a>
       </header>
     </div>
